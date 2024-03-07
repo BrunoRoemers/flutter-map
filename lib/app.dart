451779e2874cdfe5124257109a26c4a42de8940e
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2024-03-07 20:55:14 +0100

    Provide app state

---
+++ lib/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'api/country.dart';
+import 'screens/home.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        title: 'Flutter Map',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        ),
+        home: const Scaffold(
+          body: Home(),
+        ),
+      ),
+    );
+  }
+}
+
+class AppState extends ChangeNotifier {
+  final List<Country> countries = [];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'screens/home.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Map',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
-      ),
-      home: const Scaffold(
-        body: Home(),
-      ),
-    );
-  }
+  runApp(const App());
 }
